3785527ed1fe05a155765123f5fe0bcbf840cd2c
EricMTodd <eterictodd@gmail.com>
2019-11-06 21:27:07 -0700

    end of video #27

---
+++ README.md
@@ -0,0 +1,9 @@
+# udemy_python_masterclass
+
+https://www.udemy.com/course/python-the-complete-python-developer-course/
+
+### End of video #27
+
+### This is all the notes and information gathered during the length of this course that I find I must write down in order to remember it more fully.
+
+Triple quotes for strings will allow for easy usage of quotes within a string.
---
+++ udemy_python_masterclass.py
@@ -0,0 +1,25 @@
+# print("sup nerd")
+
+# greeting = "Hello"
+# name = input("Please enter your name: ")
+
+# print(f'{greeting} {name}')
+
+splitString = ("This string\nhas been\nsplit over\nseveral lines")
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5\t"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, he\'s uh... He\'s resting"')
+print("The pet shop owner said \"No, no, he's uh... He's resting\"")
+
+anotherSplitString = """This string
+has been
+split over
+several lines"""
+
+print(anotherSplitString)
+
+print("""The pet shop ownder said "No, no, he's uh... He's resting." """)
+print('''The pet shop ownder said "No, no, he's uh... He's resting." ''')
