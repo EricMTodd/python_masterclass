9e35949ed2110cc7bde8419167184cd39dd25fc0
-thugnasty- <eterictodd@gmail.com>
2020-01-06 13:34:53 -0700

    watcing video #43

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ https://www.udemy.com/course/python-the-complete-python-developer-course/
 
 ### Watching video #43. More Advanced If, Elif & Else Processing
 
-### Current timestamp - 00:00:00
+### Current timestamp - 00:06:36
 
 Python variables names must begin witha letter (either upper or lower case) or an underscore \_ character
 
@@ -13,3 +13,7 @@ They can contain letters, numbers or underscore characters (but cannot begin wit
 Python variables are case sensitive, so greeting and Greeting would refer to 2 different variables.
 
 Variables are created when they are first attached to a value, using =.
+
+When comparing conditions using AND, python stops checking once it finds a condition that's False.
+
+When comparing conditions using OR, python stops checking once it findns a condition that's True.
--- ifprogramflow.py
+++ ifprogramflow.py
@@ -1,48 +1,10 @@
-# name = input("Please enter your name: ")
-# age = int(input("How old are you, {0}? ".format(name)))
-# print(age)
+age = int(input("How old are you? "))
 
-# if age >= 18:
-#     print("You are old enough to vote!")
-#     print("Please put an X in the box.")
-# else:
-#     print("Please come back in {0} years.".format(18 - age))
+# # if (age >= 16) and (age <= 65):
+# if 15 < age < 66:
+#     print("Have a good day at work.")
 
-
-print("Please guess a number between 1 and 10: ")
-guess = int(input())
-
-if guess < 5:
-    print("Please guess higher")
-    guess = int(input())
-    if guess == 5:
-        print("Well done, you guessed it")
-    else:
-        print("Sorry, you have not guessed correctly.")
-elif guess > 5:
-    print("Please guess lower")
-    guess = int(input())
-    if guess == 5:
-        print("Well done, you guessed it")
-    else:
-        print("Sorry, you have not guessed correctly")
+if (age < 16) or (age > 65):
+    print("Enjoy your free time!")
 else:
-    print("You got it the first time")
-
-
-print("Please guess a number between 1 and 10: ")
-guess = int(input())
-
-if guess != 5:
-    if guess < 5:
-        print("Please guess higher.")
-    else:
-        print("Please guess lower.")
-
-    guess = int(input())
-    if guess == 5:
-        print("Well dont, you guessed the number!")
-    else:
-        print("Sorry, you have not guessed correctly.")
-else:
-    print("You guessed the number on your first try!")
+    print("Have a good day at work!")
